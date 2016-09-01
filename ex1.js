5638e0f699d2ae181436e1f0bfeeebe7d267ebc5
charlie_weng <charliexp.weng@gmail.com>
2016-09-01 12:41:33 +0800

    add parse timeout function
    Signed-off-by: charlie_weng <charliexp.weng@gmail.com>

--- ex1.js
+++ ex1.js
@@ -5,16 +5,16 @@ var StreamFrame = require('./');
 var server = net.createServer(function (socket) {
     var B = new StreamFrame();
     B.wrap(socket);
-    B.set('lengthSize', 1); // uint8
-    //B.set('lengthSize', 2); // uint16
+    //B.set('lengthSize', 1); // uint8
+    B.set('lengthSize', 2); // uint16
     //B.set('lengthSize', 4); // uint32
 
     //B.set('offset', 2);    // size starts at 3rd byte.
     //B.set('ignore', true); // will emit data immedietely without framing.
 
-    //B.set('bigEndian', true);  // uses bigEndian order
-    B.set('bigEndian', false); // uses default little endian order
-
+    B.set('bigEndian', true);  // uses bigEndian order
+    //B.set('bigEndian', false); // uses default little endian order
+    B.set('timeout',3000);
 
   B.on('data', function(data) {
     console.log(data);
